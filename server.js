26a30cb55ad7ec02f5284e8636f9937bf44bef24
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2018-08-22 21:45:33 +0200

    Change connect

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<dawid660816>:<dawid111>@ds125482.mlab.com:25482/database660');
+mongoose.connect('mongodb://<dawid660816>:<dawid111>@ds125482.mlab.com:25482/database660', {
+    useMongoClient: true
+});
 
 //new user Schema
 const userSchema = new Schema({
